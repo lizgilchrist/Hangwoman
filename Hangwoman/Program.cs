c1d0bfcb5777624fb1c40b72395d1697d025617a
Liz Gilchrist <lizgilchrist17@gmail.com>
2017-09-25 09:52:24 +1300

    Saving guessed letter

--- Hangwoman/Program.cs
+++ Hangwoman/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Hangwoman
 {
@@ -19,15 +21,88 @@ namespace Hangwoman
             string secretWord = Console.ReadLine();
             Console.Clear();
 
-            foreach(char i in secretWord)
+            foreach (char letter in secretWord)
             {
-                string displaySecretWord = "_" + " ";
-                Console.Write(displaySecretWord);
+                string secretWordDisplay = "_" + " ";
+                Console.Write(secretWordDisplay);
             }
-            Console.WriteLine();
-            Console.WriteLine(challenger + " take a guess at the secret word by choosing any letter of the alphabet");
-            
-            //NOTE: Replace every letter with a _ dash on screen
+
+            char correctLetter;
+
+            List<char> guesses = new List<char>();
+            //the loop will end when either the secretWord has been guessed or the woman is hung
+            while (true)
+            {
+
+                Console.WriteLine();
+                Console.WriteLine(challenger + " please guess any letter of the alphabet");
+                char guessedLetter = GetGuessedLetter();
+
+                guesses.Add(guessedLetter);
+
+                if (IsInWord(secretWord, guessedLetter))
+                {
+                    Console.WriteLine("You guessed it! " + guessedLetter + " is a letter from the secret word!");
+                    correctLetter = guessedLetter;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry but " + guessedLetter + " is not a letter from the secret word! Please try again");
+                }
+
+
+                SecretWordDisplay(secretWord, guesses);
+
+            }
+
+        }
+
+        public static void SecretWordDisplay(string secretWord, List<char> guesses)
+        {
+            foreach (char letter in secretWord)
+            {
+                if(guesses.Contains(letter))
+                {
+                    Console.Write(letter + " ");
+                }
+                else
+                {
+                    string secretWordDisplay = "_" + " ";
+                    Console.Write(secretWordDisplay);
+                }
+                
+            }
+        }
+
+        private static bool IsInWord(string secretWord, char guessedLetter)
+        {
+            return secretWord.Any(letter => letter == guessedLetter);
+            //return secretWord.Contains(guessedLetter.ToString());
+            /*foreach (char letter in secretWord)
+            {
+                if (letter == guessedLetter)
+               {
+                    return true;
+               }
+            }
+            return false;
+            */
+        }
+
+
+        private static char GetGuessedLetter()
+        {
+            string guess = Console.ReadLine();
+            char guessedLetter = guess[0];
+            //TODO: Validations in here
+            while (!char.IsLetter(guessedLetter))
+            {
+                Console.WriteLine("Sorry, that's not a valid answer. Please key in a letter of the alphabet (a-z)");
+                guess = Console.ReadLine();
+                guessedLetter = guess[0];
+            }
+
+            return guessedLetter;
         }
     }
 }
