38c02ab1a844f5430e731f914466090e8e70d740
Liz Gilchrist <lizgilchrist17@gmail.com>
2017-09-26 11:10:53 +1300

    results

--- Hangwoman/Program.cs
+++ Hangwoman/Program.cs
@@ -16,7 +16,7 @@ namespace Hangwoman
             string challenger = Console.ReadLine();
             Console.WriteLine("Great! It's " + leader + " vs " + challenger + "! LETS START!");
             Console.WriteLine();
-            //NOTE: Whoever wins will start and it's their turn to chose each word until they lose.
+            //NOTE: Whoever won last round this is where they will start - print current score
             Console.WriteLine(leader + ", please enter your secret word into the computer");
             string secretWord = Console.ReadLine();
             Console.Clear();
@@ -28,11 +28,11 @@ namespace Hangwoman
             }
 
             char correctLetter;
-
+           
             int numberOfIncorrectGuesses = 0;
 
             List<char> guesses = new List<char>();
-            //the loop will end when either the secretWord has been guessed or the woman is hung
+
             while (!IsSecretWordGuessed(secretWord, guesses) || numberOfIncorrectGuesses > 10)
             {
                 Console.WriteLine();
@@ -54,6 +54,63 @@ namespace Hangwoman
 
                 SecretWordDisplay(secretWord, guesses);
 
+            }
+
+            if(numberOfIncorrectGuesses > 10)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Unfortunately, " + challenger + " you havn't guessed the secret word in time! You've lost this one :( ");
+                Console.WriteLine("Would you like to continue playing? (Yes/No)");
+                string playAgain = Console.ReadLine().ToLower();
+
+                while (playAgain != "yes" && playAgain != "no")
+                {
+                    Console.WriteLine("Sorry, that's not a valid answer. Please try again.");
+                    playAgain = Console.ReadLine().ToLower();
+                }
+
+                if (playAgain == "yes")
+                {
+                    Console.WriteLine("That's the spirit, " + challenger + "!");
+                    //Start new round and display score at the start of it
+                }
+                else
+                {
+                    Console.WriteLine("The game's over!");
+                    //Display final score - number of wins = number of times as leader. 
+                    Console.WriteLine("Goodbye! Until next time...");
+                }
+            }
+
+            if(IsSecretWordGuessed(secretWord, guesses))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Congratulations, " + challenger + " you guessed the secret word '" + secretWord + "' in time! You are now the new leader");
+
+                var oldLeader = leader;
+                leader = challenger;
+                challenger = oldLeader;
+
+                Console.WriteLine("Would you like to continue playing? (Yes/No)");
+                string playAgain = Console.ReadLine().ToLower();
+
+                while (playAgain != "yes" && playAgain != "no")
+                {
+                    Console.WriteLine("Sorry, that's not a valid answer. Please try again.");
+                    playAgain = Console.ReadLine().ToLower();
+                }
+
+                if (playAgain == "yes")
+                {
+                    Console.WriteLine("It's your turn now to be leader, " + leader + ". " + oldLeader + " you are now the challenger. ");
+                    //Start new round and display score at the start of it
+                }
+                else
+                {
+                    Console.WriteLine("The game's over!");
+                    //Display final score - number of wins = number of times as leader. 
+                    Console.WriteLine("Goodbye! Until next time...");
+                }
             }
 
         }
